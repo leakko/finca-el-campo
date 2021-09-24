c7011d418fb9e2bf945308858923af7c7914ec42
Marcos Salvo <marcossalvo95@gmail.com>
2021-09-24 18:15:41 +0200

    Starting with HomePage component

---
+++ src/Components/HomePage/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./HomePage.scss"
+
+const HomePage = () => {
+    return (
+        <div className="">
+            <h1>Finca El Campo</h1>
+        </div>
+    );
+};
+
+export default HomePage;
