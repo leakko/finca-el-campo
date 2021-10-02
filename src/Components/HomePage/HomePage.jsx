8abc01fd63fd02e7c3097b3a7f3a1ed77a77e8d6
Marcos Salvo <marcossalvo95@gmail.com>
2021-10-02 09:50:41 +0200

    PLaceholder image source updated

--- src/Components/HomePage/HomePage.jsx
+++ src/Components/HomePage/HomePage.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Link } from "react-router-dom"
 import { Container, Button } from "react-bootstrap"
+import fincaImg from "../../Assets/finca.png"
 import "./HomePage.scss"
 
 const HomePage = () => {
     return (
         <div className="HomePage">
             <Container style={{marginTop: "25px"}}>
-                <img src="https://lh3.googleusercontent.com/proxy/kkF41WXNCrsq5LHwnGtyFINyl1ervZTV0a55d309zp4wrNXRDlCB8fRdVFMwD7PkrxSBujl6xDsoD-mWgPKaS1oG1VoEVzq8MYnH8A4SM4b--4h-9HaA" style={{marginBottom: "50px"}} />
+                <img src={fincaImg} style={{marginBottom: "50px", width: "300px"}} alt="Finca El Campo placeholder"/>
                 <Link to="/calendar"><Button>Reserva tu fecha</Button></Link>
             </Container>
         </div>
